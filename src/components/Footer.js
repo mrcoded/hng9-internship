c7a85b1ed12d92deda3346bacdb64fdd870ac5be
codedlibra <mrcoded98@gmail.com>
2022-10-29 19:49:13 -0700

    styled Footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,24 +7,29 @@ function Footer() {
   return (
     <>
     <div className='container'>
-      <div className=''>
-        <div className=''>
-          <div className=''>
-
           <div className='footerContent'>
           <div class="row d-flex justify-content-between">
-            <div class="col-sm-3"><img src={Vector} />
-            <img src={Vector_2} className='Vector_2'/>
+            <div class="col-sm-3">
+                <img src={Vector} />
+                <img src={Vector_2}
+                className='Vector_2'/>
             </div>
+
             <div class="col-6">
             <div className='footerText'> 
               HNG Internship 9 Frontend Task
             </div>
             </div>
+
              <div className="col-sm-2">
               <img src={I4G}/> 
             </div>
+            
             </div>
           </div>
         </div>
-      </div>
+</>
+  )
+}
+
+export default Footer
